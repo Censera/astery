0bb3d68e322622eec47b9e5fbdafc0907050f647
Censera <Censera@proton.me>
2026-09-13 05:37:29 +0100

    feat: expose macro parsers through compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -5,12 +5,12 @@ use crate::cast_pointer::{
 use crate::embed::{EmbeddedBlock, parse_embedded_blocks};
 use crate::error::{Error, Stage};
 use crate::lexer::{Token, tokenize};
-use crate::macros::{MacroCall, MacroDeclaration, parse_macro_call, parse_macros};
 use crate::parser::{
     BindingDeclaration, EnumDeclaration, FunctionDeclaration, Import, IntoImplementation,
     ModuleDeclaration, StructDeclaration, parse_bindings, parse_enums, parse_functions,
     parse_imports, parse_intos, parse_module, parse_structs,
 };
+use crate::shortcuts::{MacroCall, MacroDeclaration, parse_macro_call, parse_macros};
 use crate::user_type::{UserTypeDeclaration, parse_user_types};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
