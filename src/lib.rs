4d79127455634d98647bf0320f28c0a085d153a6
Censera <Censera@proton.me>
2026-09-13 04:46:39 +0100

    parser: expose enum AST

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,10 @@ pub use handler::Handler;
 pub use lexer::{Token, TokenKind};
 pub use module::Module;
 pub use parser::{
-    BinaryOperator, Binding, BindingDeclaration, BindingKind, Block, Expression, ForStatement,
-    FunctionDeclaration, IfStatement, Import, ImportItem, LoopStatement, MatchArm, MatchStatement,
-    ModuleDeclaration, Parameter, Statement, UnaryOperator, WhileStatement,
+    BinaryOperator, Binding, BindingDeclaration, BindingKind, Block, EnumDeclaration, EnumField,
+    EnumVariant, EnumVariantKind, Expression, ForStatement, FunctionDeclaration, IfStatement,
+    Import, ImportItem, LoopStatement, MatchArm, MatchStatement, ModuleDeclaration, Parameter,
+    Statement, UnaryOperator, Visibility, WhileStatement,
 };
 
 #[cfg(test)]
