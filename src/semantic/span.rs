c1abf3e8ea2689858e9923d04adc06e96a629900
Censera <Censera@proton.me>
2026-09-14 04:29:18 +0100

    Updated semantic/span.rs, defined semantic source spans

--- src/semantic/span.rs
+++ src/semantic/span.rs
@@ -20,4 +20,32 @@ impl SourceSpan {
     pub const fn new(start: SourceLocation, end: SourceLocation) -> Self {
         Self { start, end }
     }
+
+    pub const fn point(location: SourceLocation) -> Self {
+        Self::new(location, location)
+    }
+
+    pub const fn covering(start: SourceLocation, end: SourceLocation) -> Self {
+        Self::new(start, end)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Spanned<T> {
+    pub value: T,
+    pub span: SourceSpan,
+}
+
+impl<T> Spanned<T> {
+    pub fn new(value: T, span: SourceSpan) -> Self {
+        Self { value, span }
+    }
+
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+
+    pub fn span(&self) -> SourceSpan {
+        self.span
+    }
 }
